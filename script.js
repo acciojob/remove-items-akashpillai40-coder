db97ded9abc3c7eb9a3a5411cbe8d8a044b441da
Akash <akashpillai40@gmail.com>
2026-04-17 10:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let select = document.querySelector("#colorSelect");
+let button = document.querySelector("input");
+
+
+button.addEventListener("click", () => {
+  let index = select.index;
+  select.remove(index);
+});
+
